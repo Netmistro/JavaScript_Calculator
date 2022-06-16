0b07cc6380d3b1baa4506379a211cf35af116db0
Arnold Bradshaw <arnoldbradshaw@hotmail.com>
2022-06-16 13:38:08 -0400

    Complete

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const tempDisplayResult = document.querySelector('.temp-result');
 const numbersElement = document.querySelectorAll('.number');
 const operationElement = document.querySelectorAll('.operation');
 const equalElement = document.querySelector('.equal');
-const clearElement = document.querySelector('.all-clear');
+const clearAllElement = document.querySelector('.all-clear');
 const clearLastElement = document.querySelector('.last-entity-clear');
 
 let displayNum1 = '';
@@ -29,5 +29,121 @@ numbersElement.forEach((number) => {
 
 // Function to do operations
 operationElement.forEach((operation) => {
-  operation.addEventListener('click', (e) => {});
+  operation.addEventListener('click', (e) => {
+    if (!displayNum2) result;
+    haveDot = false;
+    const operationName = e.target.innerText;
+    if (displayNum1 && displayNum2 && lastOperation) {
+      mathOperation();
+    } else {
+      result = parseFloat(displayNum2);
+    }
+    clearVar(operationName);
+    lastOperation = operationName;
+  });
 });
+
+// Clear Value/Variable
+function clearVar(name = '') {
+  displayNum1 += displayNum2 + '' + name + '';
+  display1Element.innerText = displayNum1;
+  display2Element.innerText = '';
+  displayNum2 = '';
+  tempDisplayResult.innerText = result;
+}
+
+// Math Operations
+function mathOperation() {
+  if (lastOperation === 'x') {
+    result = parseFloat(result) * parseFloat(displayNum2);
+  } else if (lastOperation === '+') {
+    result = parseFloat(result) + parseFloat(displayNum2);
+  } else if (lastOperation === '-') {
+    result = parseFloat(result) - parseFloat(displayNum2);
+  } else if (lastOperation === '/') {
+    result = parseFloat(result) / parseFloat(displayNum2);
+  } else if (lastOperation === '%') {
+    result = parseFloat(result) % parseFloat(displayNum2);
+  }
+}
+
+// Functionality for the Equal Button
+equalElement.addEventListener('click', (e) => {
+  if (!displayNum1 || !displayNum2) return;
+  haveDot = false;
+  mathOperation();
+  clearVar();
+  display2Element.innerText = result;
+  tempDisplayResult.innerText = '';
+  displayNum2 = result;
+  displayNum1 = '';
+});
+
+// Clear All
+clearAllElement.addEventListener('click', (e) => {
+  display1Element.innerText = '0';
+  display2Element.innerText = '0';
+  displayNum1 = '';
+  displayNum2 = '';
+  result = '';
+  tempDisplayResult = '0';
+});
+
+// Clear last element
+clearLastElement.addEventListener('click', (e) => {
+  display1Element.innerText = '';
+  displayNum2 = '';
+});
+
+// Keyboard functionality here
+window.addEventListener('keydown', (e) => {
+  if (
+    e.key === '0' ||
+    e.key === '1' ||
+    e.key === '2' ||
+    e.key === '3' ||
+    e.key === '4' ||
+    e.key === '5' ||
+    e.key === '6' ||
+    e.key === '7' ||
+    e.key === '8' ||
+    e.key === '9' ||
+    e.key === '.'
+  ) {
+    clickButtonElement(e.key);
+  } else if (
+    e.key === '+' ||
+    e.key === '-' ||
+    e.key === '/*' ||
+    e.key === '%'
+  ) {
+    clickOperation(e.key);
+  } else if (e.key === '*') {
+    clickOperation('x');
+  } else if (e.key === 'Enter' || e.key === '=') {
+    clickEqual();
+  }
+});
+
+// Keypress function for Keyboard - Numbers
+function clickButtonElement(key) {
+  numbersElement.forEach((button) => {
+    if (button.innerText === key) {
+      button.click();
+    }
+  });
+}
+
+// Keypress function for keyboard - Operations
+function clickOperation(key) {
+  operationElement.forEach((button) => {
+    if (button.innerText === key) {
+      button.click();
+    }
+  });
+}
+
+// Keypress function for keyboard - Equal Sign
+function clickEqual(key) {
+  equalElement.click();
+}
